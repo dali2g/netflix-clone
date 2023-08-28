19586704c423fa2eabe6f252b3c89aa95a9901cf
dali2g <mamejdi12@gmail.com>
2023-08-28 03:35:37 +0100

    Finishing Signin Page layout / Prisma schema init / next-auth sign in integration

---
+++ app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,46 @@
+import type { NextAuthOptions } from 'next-auth'
+import GithubProvider from 'next-auth/providers/github'
+import Credentials from 'next-auth/providers/credentials'
+import { compare } from 'bcrypt'
+
+export const options: NextAuthOptions = {
+        providers: [
+                GithubProvider({
+                        clientId: process.env.GITHUB_ID as string,
+                        clientSecret: process.env.GITHUB_SECRET as string,
+                }),
+                Credentials({
+                        id: 'credentials',
+                        name: 'Credentials',
+                        credentials: {
+                                email: {
+                                        label: 'Email',
+                                        type: 'text',
+                                },
+                                password: {
+                                        label: 'Password',
+                                        type: 'password',
+                                }
+                        },
+                        async authorize(credentials) {
+                                if (!credentials?.email || !credentials?.password) {
+                                        throw new Error("Email and Password Required!");
+                                }
+                                const user = await prismadb.user.findUnique({
+                                        where: {
+                                                email: credentials.email
+                                        }
+                                });
+                                if (!user || !user.hashedPassword) {
+                                        throw new Error("Email does not exist");
+                                }
+                                const isCorrectPassword = await compare(user.hashedPassword, credentials.password)
+                                if (!isCorrectPassword) {
+                                        throw new Error("Incorrect Password");
+
+                                }                       
+                                return user;
+                        }
+                })
+        ],
+}
